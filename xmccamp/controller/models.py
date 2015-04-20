bd232340154d4de204fb2eb1806b5b4d0470ee54
OsamaRasheed <osamarasheedofficial@gmail.com>
2015-04-21 02:17:18 +0500

    ijoi

--- xmccamp/controller/models.py
+++ xmccamp/controller/models.py
@@ -2,17 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Parent(models.Model):
+    product = models.ForeignKey('products.Product',
+                                limit_choices_to={'parent': None})
+    
 class Cadet(models.Model):      
     user = models.OneToOneField(User)
+
     full_name = models.CharField(max_length=255)  
-    session_name = models.CharField(max_length=255)  
-    age = models.IntegerField(max_length=255)  
-    email = models.CharField(max_length=255)  
-    state = models.CharField(max_length=255)  
-    zip_code = models.CharField(max_length=255)  
-    session_end_date = models.DateTimeField()  
-    session_start_date = models.DateTimeField()  
-    profile_picture = models.ImageField(upload_to='thumbpath', blank=True)
+    age_today = models.IntegerField(max_length=255)  
+    age_session = models.IntegerField(max_length=255, blank=True, null=True)  
+    contact_number = models.IntegerField(max_length=255, blank=True, null=True)  
+    gender = models.CharField(max_length=5, blank=True, null=True)  
+    email_address = models.CharField(max_length=255, blank=True, null=True)  
+    city = models.CharField(max_length=255, blank=True, null=True)  
+    country = models.CharField(max_length=255, blank=True, null=True)  
+    
+    dob = models.DateTimeField(blank=True, null=True)  
+    state = models.CharField(max_length=255, blank=True, null=True)  
+    zip_code = models.CharField(max_length=255, blank=True, null=True)  
+
+    primary_parent = models.ForeignKey(Parent, related_name='primary_parent')
+    secondary_parent = models.ForeignKey(Parent, related_name='secondary_parent')
+    address = models.TextField(blank=True, null=True, verbose_name="Address")
+    
+    usac_training_program = models.CharField(max_length=255, blank=True, null=True)  
+    goal = models.TextField(blank=True, null=True, verbose_name="Why join XMC Camp?")
+    
 
     def __unicode__(self):
-        return u'Profile of user: %s' % self.user.username
+        return u'Cadet Profile of user: %s' % self.user.username
